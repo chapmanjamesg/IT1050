3ab3d835535a000c87032e731c102108f4e27d7c
James Chapman <chapmanjamesg@gmail.com>
2017-09-19 15:18:04 -0400

    finishing my Lab

---
+++ Lab 1/Lab 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.Write("What is your First Name?");
+            string firstName = Console.ReadLine();
+            Console.Write("What is your Middle Initial?");
+            string middleInitial = Console.ReadLine();
+            Console.Write("What is your Last Name?");
+            string lastName = Console.ReadLine();
+            string fullName = firstName + " " + middleInitial + ". " + lastName;
+            Console.Write("How tall are you in Feet?");
+            int heightFeet = int.Parse(Console.ReadLine());
+            Console.Write("How many inches remain?");
+            double heightInches = double.Parse(Console.ReadLine());
+            double totalHeightCM;
+            totalHeightCM = (((heightFeet * 12) + heightInches) * 2.54);
+            Console.Write("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("True or False: Are you a US Citizen?");
+            bool isCitizen = bool.Parse(Console.ReadLine());
+            bool canVote = (age >= 18) == isCitizen;
+            Console.WriteLine("Your full name:" + fullName);
+            Console.WriteLine("Your Height in Centimeters:" + totalHeightCM);
+            Console.WriteLine("Are you able to vote:" + canVote);
+            Console.WriteLine("Press any Button to Continue...");
+            Console.ReadKey();
+        }
+    }
+}
